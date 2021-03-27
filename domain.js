f971ec20234ed1f4624bd1a9ca3d63d991002f8e
AATG-ui <76041889+AATG-ui@users.noreply.github.com>
2021-03-27 14:45:53 +0530

    Add files via upload

---
+++ domain.js
@@ -0,0 +1,34 @@
+function gmail()
+{
+    window.location = "https://mail.google.com/mail/?authuser=0&ogbl";
+}
+
+function image()
+{
+    window.location = "https://www.google.co.in/imghp?hl=en&authuser=0&ogbl&safe=active";
+}
+
+function about()
+{
+    window.location = "https://about.google/?utm_source=google-IN&utm_medium=referral&utm_campaign=hp-footer&fg=1";
+}
+
+function store()
+{
+    window.location = "https://store.google.com/";
+}
+
+function search()
+{
+    window.location = "https://www.google.co.in/?safe=active";
+}
+
+function lucky()
+{
+    window.location = "https://www.google.com/doodles?safe=active";
+}
+
+function abhijeet()
+{
+    window.location = "https://www.linkedin.com/in/abhijeetv/";
+}
